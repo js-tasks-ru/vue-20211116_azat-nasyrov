83f05a572b3fa9e98c0b4f29149c013a0a58fa94
Azat Nasyrov <azat.nasyrov31@gmail.com>
2021-11-23 13:55:32 +0600

    Increase counter

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,22 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+
+const RootComponent = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter++;
+    },
+  },
+});
+
+const app = createApp(RootComponent);
+
+const vm = app.mount('#app');
 
 // Создайте Vue приложение
